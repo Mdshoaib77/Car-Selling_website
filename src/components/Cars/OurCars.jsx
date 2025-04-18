db18c04036379da18c54864307d4781d6c894901
Mdshoaib77 <email4966@gmail.com>
2025-04-18 09:01:59 +0600

    Update OurCars.jsx

--- src/components/Cars/OurCars.jsx
+++ src/components/Cars/OurCars.jsx
@@ -6,37 +6,37 @@ const OurCars = () => {
   const carsData = [
     {
       id: 0,
-      img: "/src/assets/img/car1.png",
+      img: "/assets/img/car1.png",
       name: "Cadillac Escalade",
       price: "22,440",
     },
     {
       id: 1,
-      img: "/src/assets/img/car2.png",
+      img: "/assets/img/car2.png",
       name: "BMW 3 Series",
       price: "54,890",
     },
     {
       id: 2,
-      img: "/src/assets/img/car3.png",
+      img: "/assets/img/car3.png",
       name: "Mercedes",
       price: "75,890",
     },
     {
       id: 3,
-      img: "/src/assets/img/car4.png",
+      img: "/assets/img/car4.png",
       name: "BMW 7 Series",
       price: "55,789",
     },
     {
       id: 4,
-      img: "/src/assets/img/car5.png",
+      img: "/assets/img/car5.png",
       name: "Mercedes-Benz",
       price: "95,776",
     },
     {
       id: 5,
-      img: "/src/assets/img/car6.png",
+      img: "/assets/img/car6.png",
       name: "Range Rover",
       price: "88,450",
     },
